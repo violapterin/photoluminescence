4c7e05a55de375a9c51540d75ba39a1311885f2d
Raven I. Pilot <violapterin@gmail.com>
2021-06-13 03:26:05 +0800

    Save: Produce data of post separately

--- make-site.py
+++ make-site.py
@@ -21,7 +21,7 @@ def main():
    text_entries = AID.input_file(path_entries)
    entries = json.loads(text_entries)
    entries = [entry for entry in entries if entry is not None]
-   order = lambda entry: entry.get("date")
+   order = lambda entry: entry.get("stamp")
    entries.sort(key = order, reverse = True)
 
    # # Convert posts into HTML article.
@@ -31,35 +31,43 @@ def main():
    # # # # # # # # # # # # # # # #
    # # Combine HTML articles into HTML.
    path_head = os.path.join(folder_matter, "head.html")
-   path_banner = os.path.join(folder_matter, "banner.html")
    path_header = os.path.join(folder_matter, "header.html")
    path_footer = os.path.join(folder_matter, "footer.html")
    path_item = os.path.join(folder_matter, "item.html")
    head = AID.input_file(path_head)
-   banner = AID.input_file(path_banner)
    header = AID.input_file(path_header)
    footer = AID.input_file(path_footer)
    item = AID.input_file(path_item)
 
    for entry in entries:
       wholes = []
-      date = entry.get("date")
-      title = entry.get("title")
-      genres = entry.get("genres")
-      tags = entry.get("tags")
+      heading = entry.get("heading")
+      stamp = entry.get("stamp")
+      genres = create_group(entry.get("genre"))
+      tags = create_group(entry.get("tag"))
       series = entry.get("series")
-      data = {
-         "$DATE": get_text_date(date),
-         "$TITLE": title,
-         "$GENRES": join_with_comma(genres),
-         "$TAGS": join_with_comma(tags),
-         "$SERIES": join_with_comma(series),
-      }
+
+      items = []
+      if stamp is not None:
+         items.append(write_element_stamp(stamp))
+      if genres is not [None]:
+         elements = [write_element_genre(genre) for genre in genres]
+         items.extend(elements)
+      if tags is not [None]:
+      items.extend([write_element_tag(tag) for tag in tags])
+      if series is not None:
+         items.append(write_element_series(series))
+      item = '\n'.join(items)
+
+      element_heading = "<heading>{}</heading>".format(heading)
+      head_plugged = head.replace("$TITLE", element_heading)
+
+      month = get_month()
 
       path_output = ''
       path_post = ''
       for name in os.listdir(folder_output):
-         if name.startswith(date):
+         if name.startswith(stamp):
             path_output = os.path.join(folder_output, name)
             bare = name.split('.')[0]
             if not bare:
@@ -69,10 +77,9 @@ def main():
       output = AID.input_file(path_output)
 
       wholes.append("<html>")
-      wholes.append(plug_head(head, data))
+      wholes.append(head_plugged)
       wholes.append("<body>")
-      wholes.append(banner)
-      wholes.append(plug_header(header, data))
+      wholes.append(header_plugged)
       wholes.append(output)
       wholes.append(footer)
       wholes.append("</body>")
@@ -92,25 +99,17 @@ def main():
    wholes_home.append(banner)
 
    for entry in entries_newest:
-      date = entry.get("date")
-      title = entry.get("title")
+      stamp = entry.get("stamp")
+      heading = entry.get("heading")
       genres = entry.get("genres")
       tags = entry.get("tags")
       series = entry.get("series")
       name_post = ''
       for name in os.listdir(folder_post):
-         if name.startswith(date):
+         if name.startswith(stamp):
             name_post = name
             break
 
-      data = {
-         "$NAME": name_post,
-         "$DATE": get_text_date(date),
-         "$TITLE": title,
-         "$GENRES": join_with_comma(genres),
-         "$TAGS": join_with_comma(tags),
-         "$SERIES": join_with_comma(series),
-      }
       plug_item(item, data)
       wholes_home.append(plug_item(item, data))
 
@@ -121,25 +120,100 @@ def main():
    path_index = os.path.join(folder_site, "index.html")
    AID.output_file(path_index, whole_home)
 
+   # # # # # # # # # # # # # # # #
+   # # Update pages.
+
+
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-def get_text_date(date):
+
+def get_day(stamp):
+   day = stamp[4:6]
+   if (day[0] == '0'):
+      day = day[1]
+   return day
+
+def get_month(stamp):
    months = {
       "01": "January", "02": "February", "03": "March",
       "04": "April", "05": "May", "06": "June",
       "07": "July", "08": "August", "09": "September",
       "10": "October", "11": "November", "12": "December",
    }
-   year = "20" + date[0:2]
-   month = months.get(date[2:4])
-   day = date[4:6]
-   if (day[0] == '0'):
-      day = day[1]
-   text = ' '.join([month, day + ',', year])
-   return text
+   month = months.get(stamp[2:4])
+   return month
 
+def get_year(stamp):
+   year = "20" + stamp[0:2]
+   return year
+
+def get_date(stamp):
+   month = get_month(stamp)
+   day = get_day(stamp)
+   year = get_year(stamp)
+   date = ' '.join([month, day + ',', year])
+   return date
+
+
+def write_heading_post(heading):
+   if not heading:
+      return None
+   sinks = "<h1 class=\"heading-post\">{}</h1>".format(heading)
+   return sink
+
+def write_element_stamp(stamp):
+   if not stamp:
+      return None
+   sinks = (
+      "<a class=\"data-stamp\"" + ' '
+      "href=\"/stamp/{}\">{}</a>"
+   ).format(unify_link(get_month(stamp)), date)
+   return sink
+
+def write_element_genre(genre):
+   if not genre:
+      return None
+   sink = (
+      "<a class=\"data-genre\"" + ' '
+      "href=\"/genre/{}\">{}</a>"
+   ).format(unify_link(genre), genre)
+   return sink
+
+def write_element_tag(tag):
+   if not tag:
+      return None
+   sink = (
+      "<a class=\"data-tag\"" + ' '
+      "href=\"/tag/{}\">{}</a>"
+   ).format(unify_link(genre), genre)
+   return sink
+
+def write_element_series(series):
+   if not series:
+      return None
+   sink = (
+      "<a class=\"data-series\"" + ' '
+      "href=\"/series/{}\">{}</a>"
+   ).format(unify_link(series), series)
+   return sink
+
+def unify_link(source):
+   sink = lower(source)
+   sink = sink.replace(' ', '-')
+   return sink
+
+def create_group(item):
+   group = []
+   if (isinstance(group, list)):
+      group = item
+   else:
+      group = [item]
+   return group
+
+
+'''
 def join_with_comma(items):
    item = ''
    if (isinstance(items, list)):
@@ -148,11 +222,10 @@ def join_with_comma(items):
       item = items
    return item
 
-
 def plug_item(source, values):
    keys = {
       "$NAME",
-      "$DATE",
+      "$STAMP",
       "$TITLE",
       "$GENRES",
       "$TAGS",
@@ -162,36 +235,9 @@ def plug_item(source, values):
    sink = plug_value(source, values_part)
    return sink
 
-def write_date(method, value): # XXX
-   sink = ''
-   sink += "<span class=\"button-{}\">".format(method)
-   anchor = "<a href=\"/{}/{}-{}.html\">{}</a>"
-   sink += anchor.format(method, method, value, value)
-   sink += "</span>"
-   return sink
-
-def write_catalog(method, value):
-   sink = ''
-   sink += "<span class=\"button-{}\">".format(method)
-   anchor = "<a href=\"/{}/{}-{}.html\">{}</a>"
-   sink += anchor.format(method, method, value, value)
-   sink += "</span>"
-   return sink
-
-def write_item(data):
-   sinks = []
-   sinks.append("<h1 class=\"title-post\">{}</h1>".format(title))
-   sinks.append(write_date(data))
-   sinks.append(write_catalog(data, value))
-   sink = '\n'.join(sinks)
-   return sink
-
-def write_title(title):
-   return "<title>{}</title>".format(title)
-
 def plug_header(source, values):
    keys = {
-      "$DATE",
+      "$STAMP",
       "$TITLE",
       "$GENRES",
       "$TAGS",
@@ -219,6 +265,7 @@ def plug_value(source, values):
       else:
          sink = sink.replace(key, value)
    return sink
+'''
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
