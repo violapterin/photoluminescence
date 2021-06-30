092d89c3608a760b7895db0c05e87d0da18dde25
Raven I. Pilot <violapterin@gmail.com>
2021-07-01 02:21:26 +0800

    Rename variables

--- make-site.py
+++ make-site.py
@@ -14,7 +14,7 @@ def main():
    folder_site = os.path.join(folder_this, "site")
    folder_post = os.path.join(folder_site, "post")
    folder_page = os.path.join(folder_site, "page")
-   records = load_record(folder_this)
+   many_record = load_record(folder_this)
    matter = load_matter(folder_matter)
 
    # # # # # # # # # # # # # # # #
@@ -24,7 +24,7 @@ def main():
 
    # # # # # # # # # # # # # # # #
    # # Write posts.
-   for record in records:
+   for record in many_record:
       stamp = record.get("stamp")
       path_plain = search_path_plain(folder_plain, stamp)
       plain = AID.input_file(path_plain)
@@ -34,98 +34,98 @@ def main():
       AID.output_file(path_post, whole)
 
    # # # # # # # # # # # # # # # #
-   # # Update catalogs.
-   catalogs_stamp = {}
-   for record in records:
+   # # Update many_catalog.
+   many_catalog_stamp = {}
+   for record in many_record:
       stamp = record.get("stamp")
       month = get_month(stamp)
       if not month:
          continue
-      if month not in catalogs_stamp:
-         catalogs_stamp[month] = [record]
+      if month not in many_catalog_stamp:
+         many_catalog_stamp[month] = [record]
          continue
-      catalogs_stamp[month].append(record)
+      many_catalog_stamp[month].append(record)
    heading = "By date"
-   whole = write_catalog(matter, catalogs_stamp, folder_post, heading)
+   whole = write_catalog(matter, many_catalog_stamp, folder_post, heading)
    path_stamp = os.path.join(folder_site, "stamp.html")
    AID.output_file(path_stamp, whole)
 
-   catalogs_genre = {}
-   for record in records:
+   many_catalog_genre = {}
+   for record in many_record:
       elision = record.get("genre")
       genre = get_genre(elision)
       if not genre:
          continue
-      if genre not in catalogs_genre:
-         catalogs_genre[genre] = [record]
+      if genre not in many_catalog_genre:
+         many_catalog_genre[genre] = [record]
          continue
-      catalogs_genre[genre].append(record)
+      many_catalog_genre[genre].append(record)
    heading = "By genre"
-   whole = write_catalog(matter, catalogs_genre, folder_post, heading)
+   whole = write_catalog(matter, many_catalog_genre, folder_post, heading)
    path_genre = os.path.join(folder_site, "genre.html")
    AID.output_file(path_genre, whole)
 
-   catalogs_tag = {}
-   for record in records:
-      tags = check_group(record.get("tag"))
-      for tag in tags:
+   many_catalog_tag = {}
+   for record in many_record:
+      many_tag = check_group(record.get("tag"))
+      for tag in many_tag:
          if not tag:
             continue
-         if tag not in catalogs_tag:
-            catalogs_tag[tag] = [record]
+         if tag not in many_catalog_tag:
+            many_catalog_tag[tag] = [record]
             continue
-         catalogs_tag[tag].append(record)
+         many_catalog_tag[tag].append(record)
    heading = "By tag"
-   whole = write_catalog(matter, catalogs_tag, folder_post, heading)
+   whole = write_catalog(matter, many_catalog_tag, folder_post, heading)
    path_tag = os.path.join(folder_site, "tag.html")
    AID.output_file(path_tag, whole)
 
-   catalogs_series = {}
-   for record in records:
+   many_catalog_series = {}
+   for record in many_record:
       series = record.get("series")
       if not series:
          continue
-      if series not in catalogs_series:
-         catalogs_series[series] = [record]
+      if series not in many_catalog_series:
+         many_catalog_series[series] = [record]
          continue
-      catalogs_series[series].append(record)
+      many_catalog_series[series].append(record)
    heading = "By series"
-   whole = write_catalog(matter, catalogs_series, folder_post, heading)
+   whole = write_catalog(matter, many_catalog_series, folder_post, heading)
    path_series = os.path.join(folder_site, "series.html")
    AID.output_file(path_series, whole)
    
    # # # # # # # # # # # # # # # #
    # # Write pages.
    number_post_max = 16
-   number_post_shown = min(number_post_max, len(records))
-   records_index = records[:number_post_shown]
+   number_post_shown = min(number_post_max, len(many_record))
+   many_record_index = many_record[:number_post_shown]
    heading = "Latest"
-   whole = write_page(matter, records_index, folder_post, heading)
+   whole = write_page(matter, many_record_index, folder_post, heading)
    path_index = os.path.join(folder_site, "index.html")
    AID.output_file(path_index, whole)
 
-   for month, catalog in catalogs_stamp.items():
+   for month, catalog in many_catalog_stamp.items():
       heading = month
       name = unify_name(month) + ".html"
       whole = write_page(matter, catalog, folder_post, heading)
       path_stamp = os.path.join(folder_page, name)
       AID.output_file(path_stamp, whole)
 
-   for genre, catalog in catalogs_genre.items():
+   for genre, catalog in many_catalog_genre.items():
       heading = genre
       name = unify_name(genre) + ".html"
       whole = write_page(matter, catalog, folder_post, heading)
       path_genre = os.path.join(folder_page, name)
       AID.output_file(path_genre, whole)
 
-   for tag, catalog in catalogs_tag.items():
+   for tag, catalog in many_catalog_tag.items():
       heading = tag
       name = unify_name(tag) + ".html"
       whole = write_page(matter, catalog, folder_post, heading)
       path_tag = os.path.join(folder_page, name)
       AID.output_file(path_tag, whole)
 
-   for series, catalog in catalogs_series.items():
+   for series, catalog in many_catalog_series.items():
       heading = series
       name = unify_name(series) + ".html"
       whole = write_page(matter, catalog, folder_post, heading)
@@ -160,17 +160,17 @@ def load_matter(folder_matter):
    return matter
 
 def load_record(folder_this):
-   path_records = os.path.join(folder_this, "record.json")
-   records = json.loads(AID.input_file(path_records))
-   records = [record for record in records if record is not None]
+   path_many_record = os.path.join(folder_this, "record.json")
+   many_record = json.loads(AID.input_file(path_many_record))
+   many_record = [record for record in many_record if record is not None]
    order = (lambda record: record.get("stamp"))
-   records.sort(key = order, reverse = True)
-   return records
+   many_record.sort(key = order, reverse = True)
+   return many_record
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-def write_catalog(matter, catalogs, folder_post, heading):
-   wholes = []
+def write_catalog(matter, many_catalog, folder_post, heading):
+   many_whole = []
    head = matter["head"].replace("$TITLE", write_title(heading))
    header_banner = matter["header_banner"]
    header_page = matter["header_page"].replace(
@@ -180,30 +180,30 @@ def write_catalog(matter, catalogs, folder_post, heading):
    footer = matter["footer"]
    entry = matter["entry"]
 
-   wholes.append("<!DOCTYPE html>")
-   wholes.append("<html lang=\"en\">")
-   wholes.append(head)
-   wholes.append("<body>")
-   wholes.append(header_banner)
-   wholes.append(header_page)
-   for kind, catalog in catalogs.items():
+   many_whole.append("<!DOCTYPE html>")
+   many_whole.append("<html lang=\"en\">")
+   many_whole.append(head)
+   many_whole.append("<body>")
+   many_whole.append(header_banner)
+   many_whole.append(header_page)
+   for kind, catalog in many_catalog.items():
       display = write_element_display(kind, unify_name(kind))
-      wholes.append("<p class=\"title-catalog\">")
-      wholes.append(display)
-      wholes.append("</p>")
+      many_whole.append("<p class=\"title-catalog\">")
+      many_whole.append(display)
+      many_whole.append("</p>")
       for record in catalog:
          stamp = record.get("stamp")
          name_post = search_path_post(folder_post, stamp)
          display = write_entry(entry, record, name_post)
-         wholes.append(display)
-   wholes.append(footer)
-   wholes.append("</body>")
-   wholes.append("</html>")
-   whole = '\n\n'.join(wholes)
+         many_whole.append(display)
+   many_whole.append(footer)
+   many_whole.append("</body>")
+   many_whole.append("</html>")
+   whole = '\n\n'.join(many_whole)
    return whole
 
-def write_page(matter, records, folder_post, heading):
-   wholes = []
+def write_page(matter, many_record, folder_post, heading):
+   many_whole = []
    head = matter["head"].replace("$TITLE", write_title(heading))
    header_banner = matter["header_banner"]
    header_page = matter["header_page"].replace(
@@ -213,42 +213,42 @@ def write_page(matter, records, folder_post, heading):
    footer = matter["footer"]
    entry = matter["entry"]
 
-   wholes.append("<!DOCTYPE html>")
-   wholes.append("<html lang=\"en\">")
-   wholes.append(head)
-   wholes.append("<body>")
-   wholes.append(header_banner)
-   wholes.append(header_page)
-   for record in records:
+   many_whole.append("<!DOCTYPE html>")
+   many_whole.append("<html lang=\"en\">")
+   many_whole.append(head)
+   many_whole.append("<body>")
+   many_whole.append(header_banner)
+   many_whole.append(header_page)
+   for record in many_record:
       stamp = record.get("stamp")
       name_post = search_path_post(folder_post, stamp)
       display = write_entry(entry, record, name_post)
-      wholes.append(display)
-   wholes.append(footer)
-   wholes.append("</body>")
-   wholes.append("</html>")
-   whole = '\n\n'.join(wholes)
+      many_whole.append(display)
+   many_whole.append(footer)
+   many_whole.append("</body>")
+   many_whole.append("</html>")
+   whole = '\n\n'.join(many_whole)
    return whole
 
 def write_post(matter, plain, record):
-   wholes = []
+   many_whole = []
    title = record.get("title")
    head = matter["head"].replace("$TITLE", write_title(title))
    header_banner = matter["header_banner"]
    header_post = write_entry(matter["header_post"], record, None)
    footer = matter["footer"]
 
-   wholes.append("<!DOCTYPE html>")
-   wholes.append("<html lang=\"en\">")
-   wholes.append(head)
-   wholes.append("<body>")
-   wholes.append(header_banner)
-   wholes.append(header_post)
-   wholes.append(plain)
-   wholes.append(footer)
-   wholes.append("</body>")
-   wholes.append("</html>")
-   whole = "\n\n".join(wholes)
+   many_whole.append("<!DOCTYPE html>")
+   many_whole.append("<html lang=\"en\">")
+   many_whole.append(head)
+   many_whole.append("<body>")
+   many_whole.append(header_banner)
+   many_whole.append(header_post)
+   many_whole.append(plain)
+   many_whole.append(footer)
+   many_whole.append("</body>")
+   many_whole.append("</html>")
+   whole = "\n\n".join(many_whole)
    return whole
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
@@ -258,7 +258,7 @@ def write_entry(source, record, name):
       heading = record.get("title")
       stamp = record.get("stamp")
       elision = record.get("genre")
-      tags = check_group(record.get("tag"))
+      many_tag = check_group(record.get("tag"))
       series = record.get("series")
       if heading is not None:
          element = write_element_heading(heading, name)
@@ -280,11 +280,11 @@ def write_entry(source, record, name):
          sink = sink.replace("$SERIES", element)
       else:
          sink = sink.replace("$SERIES", '')
-      if (tags is not None) and (tags is not [None]):
-         elements = []
-         for tag in tags:
-            elements.append(write_element_tag(tag))
-         element = '\n'.join(elements)
+      if (many_tag is not None) and (many_tag is not [None]):
+         many_element = []
+         for tag in many_tag:
+            many_element.append(write_element_tag(tag))
+         element = '\n'.join(many_element)
          sink = sink.replace("$TAG", element)
       else:
          sink = sink.replace("$TAG", '')
@@ -308,9 +308,9 @@ def search_path_post(folder, stamp):
          if name.startswith(stamp):
             name_post = name
       elif os.path.isdir(path):
-         for subname in os.listdir(path):
-            if subname.startswith(stamp):
-               name_post = subname
+         for name_deep in os.listdir(path):
+            if name_deep.startswith(stamp):
+               name_post = name_deep
                break
       if name_post:
          break
