c566da978b61ea66acd5f3f333f4550d8dfb151f
Raven I. Pilot <violapterin@gmail.com>
2021-08-10 23:09:36 +0800

    Meta: Slice lines: Sketch

--- capture.py
+++ capture.py
@@ -2,6 +2,7 @@
 
 import os
 import io
+import numpy as NUMPY
 from selenium import webdriver as DRIVER
 from PIL import Image as IMAGE
 from PIL import ImageEnhance as ENHANCE
@@ -13,14 +14,64 @@ def main():
    folder_shot = os.path.join(folder_book, "shot")
    folder_slice = os.path.join(folder_book, "slice")
    folder_leaf = os.path.join(folder_book, "leaf")
-
-   many_title = extract_title(folder_cipher)
-   many_title.sort()
-   #many_title = many_title[0:4] # XXX
+   assert(os.path.isdir(folder_cipher))
+   assert(os.path.isdir(folder_book))
+   assert(os.path.isdir(folder_shot))
+   assert(os.path.isdir(folder_slice))
+   assert(os.path.isdir(folder_leaf))
+   '''
    many_title = [
-      "190711-shuri-castle-okinawa",
+      "190710-american-village-okinawa",
       "210719-valid-triangle-numbers",
    ]
+   '''
+   many_title = extract_title(folder_cipher)
+   many_title = many_title[0:1] # XXX
+   take_photograph(folder_shot, many_title)
+   shred_photograph(folder_slice, folder_shot)
+
+def shred_photograph(folder_slice, folder_shot):
+   suffix = ".png"
+   tmp = 0
+   for name_shot in os.listdir(folder_shot):
+      tmp += 1
+      if tmp >= 2:
+         break
+
+      if not name_shot.endswith(suffix):
+         continue
+      bare = name_shot[:-len(suffix)]
+      path_shot = os.path.join(folder_shot, name_shot)
+      if not os.path.isfile(path_shot):
+         continue
+      print("slicing:", path_shot, "......")
+      with IMAGE.open(path_shot) as graph:
+         matrix = NUMPY.asarray(graph.convert('L'))
+         height = matrix.shape[0]
+         width = matrix.shape[1]
+         marginal = [sum(row) for row in list(matrix)]
+         flip = [255 * width - value for value in marginal]
+         print(flip[0:20])
+         above = 0
+         below = 0
+         count_tail = 0
+         unit = 24
+         bright = int(width / 128)
+         for ruler in range(len(flip)):
+            gray = flip[ruler]
+            if (gray > bright):
+               continue
+            name_slice = bare + give_tail(count_tail) + suffix
+            path_slice = os.path.join(folder_slice, name_slice)
+            if (ruler - above >= unit):
+               below = ruler
+               print("ruler:", ruler)
+               part = graph.crop((0, above, height, below + 1))
+               part.save(path_slice, quality = 100)
+               above = below
+               count_tail += 1
+
+def take_photograph(folder_shot, many_title):
    prefix = "https://www.violapterin.com/post/"
    many_path_graph = []
    for title in many_title:
@@ -31,18 +82,18 @@ def main():
          continue
       many_path_graph.append(path_graph)
       binary = save_screenshot(address)
-      graph = IMAGE.open(io.BytesIO(binary))
-      graph = enhance_contrast(graph)
-      graph = enhance_sharpness(graph)
-      graph.save(path_graph, quality = 92)
+      with IMAGE.open(io.BytesIO(binary)) as graph:
+         graph = enhance_contrast(graph)
+         graph = enhance_sharpness(graph)
+         graph.save(path_graph, quality = 100)
 
 def enhance_sharpness(graph):
-   level = 1.3
+   level = 1.4
    graph = ENHANCE.Sharpness(graph).enhance(level)
    return graph
 
 def enhance_contrast(graph):
-   level = 1.1
+   level = 1.2
    graph = ENHANCE.Contrast(graph).enhance(level)
    return graph
 
@@ -59,11 +110,11 @@ def save_screenshot(address):
    driver.quit()
    return graph
 
-def extract_title(folder):
+def extract_title(folder_cipher):
    many_title = []
    suffix = ".txt"
-   for name in os.listdir(folder):
-      path = os.path.join(folder, name)
+   for name in os.listdir(folder_cipher):
+      path = os.path.join(folder_cipher, name)
       if os.path.isfile(path):
          if not name.endswith(suffix):
             continue
@@ -71,7 +122,16 @@ def extract_title(folder):
          many_title.append(bare)
       elif os.path.isdir(path):
          many_title.extend(extract_title(path))
+   many_title.sort()
    return many_title
+
+def give_tail(count):
+   base = 26
+   start = 97
+   small = str(chr(count % base + start))
+   big = str(chr(int(count // base) + start))
+   tail = str(big) + str(small)
+   return big + small
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
