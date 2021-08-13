73d593ca6c9e611bb9d900b51f38bdaadf8c018c
Raven I. Pilot <violapterin@gmail.com>
2021-08-13 16:51:46 +0800

    Capture: Distinguish made targets new and all

---
+++ capture-book-all.py
@@ -0,0 +1,6 @@
+#! /usr/bin/env python3
+
+import capture
+
+
+capture.main(False)
--- capture.py
+++ capture.py
@@ -1,21 +1,33 @@
-#! /usr/bin/env python3
-
 import os
 import io
 import copy
+import shutil
 import numpy as NUMPY
 from selenium import webdriver as DRIVER
 from PIL import Image as IMAGE
 from PIL import ImageEnhance as ENHANCE
 from PIL import ImageOps as OPERATION
 
-def main():
+def main(whether_new):
    folder_this = os.path.dirname(__file__)
    folder_cipher = os.path.join(folder_this, "post-cipher")
    folder_book = os.path.join(folder_this, "book")
    folder_shot = os.path.join(folder_book, "shot")
    folder_strip = os.path.join(folder_book, "strip")
    folder_leaf = os.path.join(folder_book, "leaf")
+   if not whether_new:
+      if os.path.isdir(folder_shot):
+         shutil.rmtree(folder_shot)
+      if os.path.isdir(folder_strip):
+         shutil.rmtree(folder_strip)
+      if os.path.isdir(folder_leaf):
+         shutil.rmtree(folder_leaf)
+   if not os.path.isdir(folder_shot):
+      os.mkdir(folder_shot)
+   if not os.path.isdir(folder_strip):
+      os.mkdir(folder_strip)
+   if not os.path.isdir(folder_leaf):
+      os.mkdir(folder_leaf)
    assert(os.path.isdir(folder_cipher))
    assert(os.path.isdir(folder_book))
    assert(os.path.isdir(folder_shot))
@@ -29,16 +41,17 @@ def main():
    '''
    many_title = extract_title(folder_cipher)
    many_title = many_title[0:3] # XXX
-   take_photograph(folder_shot, many_title)
-   shred_photograph(folder_strip, folder_shot)
-   patch_leaf(folder_leaf, folder_strip)
+   take_photograph(whether_new, folder_shot, many_title)
+   shred_photograph(whether_new, folder_strip, folder_shot)
+   patch_leaf(whether_new, folder_leaf, folder_strip)
 
-def patch_leaf(folder_leaf, folder_strip):
-   if not compare_folder_with_folder(folder_leaf, folder_strip):
-      return
+def patch_leaf(whether_new, folder_leaf, folder_strip):
+   if whether_new:
+      if not compare_folder_with_folder(folder_leaf, folder_strip):
+         return
    suffix_in = ".png"
    suffix_out = ".jpg"
-   limit_height = 1200
+   limit_height = constant()["inner_height"]
    total = 0
    number_page = 0
    for name_article in os.listdir(folder_strip):
@@ -52,7 +65,6 @@ def patch_leaf(folder_leaf, folder_strip):
          if not os.path.isfile(path_strip):
             continue
          with IMAGE.open(path_strip) as strip:
-            print("opening", path_strip)
             height = strip.size[1]
             if (total == 0):
                leaf = copy.copy(strip)
@@ -62,6 +74,7 @@ def patch_leaf(folder_leaf, folder_strip):
                number_page += 1
                path_leaf = os.path.join(folder_leaf, name_leaf)
                leaf = tune_leaf(leaf)
+               print("Saving leaf:", name_leaf, "......")
                leaf.save(path_leaf, quality = 100)
                leaf = copy.copy(strip)
                total = height
@@ -71,25 +84,27 @@ def patch_leaf(folder_leaf, folder_strip):
    name_leaf = str(number_page).zfill(3) + suffix_out
    path_leaf = os.path.join(folder_leaf, name_leaf)
    leaf = tune_leaf(leaf)
+   print("Saving leaf:", name_leaf, "......")
    leaf.save(path_leaf, quality = 100)
 
-def shred_photograph(folder_strip, folder_shot):
-   suffix = ".png"
+def shred_photograph(whether_new, folder_strip, folder_shot):
+   suffix_in = ".png"
+   suffix_out = ".png"
    for name_shot in os.listdir(folder_shot):
-      if not name_shot.endswith(suffix):
+      if not name_shot.endswith(suffix_in):
          continue
-      bare_graph = name_shot[:-len(suffix)]
+      bare_graph = name_shot[:-len(suffix_in)]
       path_graph = os.path.join(folder_shot, name_shot)
       if not os.path.isfile(path_graph):
          continue
       folder_article = os.path.join(folder_strip, bare_graph)
       if not os.path.isdir(folder_article):
-         os.system("mkdir" + ' ' + folder_article)
-      if not compare_folder_with_file(folder_article, path_graph):
-         continue
-      print("Slicing:", path_graph, "......")
+         os.mkdir(folder_article)
+      if whether_new:
+         if not compare_folder_with_file(folder_article, path_graph):
+            continue
       limit_bright = 255
-      height_line = 288
+      limit_line = constant()["limit_line"]
       with IMAGE.open(path_graph) as graph:
          matrix = NUMPY.asarray(graph.convert('L'))
          height = matrix.shape[0]
@@ -106,24 +121,26 @@ def shred_photograph(folder_strip, folder_shot):
                "strips.", "It is cropped."
             )
          for head in range(0, len(many_boundary) - 1):
-            name_strip = bare_graph + give_tail(head) + suffix
+            name_strip = bare_graph + give_tail(head) + suffix_out
             path_strip = os.path.join(folder_article, name_strip)
             above = many_boundary[head]
             below = many_boundary[head + 1]
             strip = graph.crop((0, above, width - 1, below))
-            if (below - above < height_line):
+            if (below - above < limit_line):
                strip = enhance_contrast(strip)
                strip = enhance_sharpness(strip)
+            print("Slicing strip:", name_strip, "......")
             strip.save(path_strip, quality = 100)
 
-def take_photograph(folder_shot, many_title):
+def take_photograph(whether_new, folder_shot, many_title):
    prefix = "https://www.violapterin.com/post/"
    many_path_graph = []
-   suffix = ".png"
+   suffix_in = ".html"
+   suffix_out = ".png"
    for title in many_title:
       stamp = title.split('-')[0]
-      address = prefix + title + ".html"
-      path_graph = os.path.join(folder_shot, stamp) + suffix
+      address = prefix + title + suffix_in
+      path_graph = os.path.join(folder_shot, stamp) + suffix_out
       if os.path.isfile(path_graph):
          continue
       many_path_graph.append(path_graph)
@@ -133,11 +150,11 @@ def take_photograph(folder_shot, many_title):
 def save_screenshot(address):
    id_heading = "header-post"
    class_content = "document"
-   size = 1536
+   size = constant()["outer_width"]
    os.environ["MOZ_HEADLESS"] = "1"
-   print("Capturing:", address, "......")
    driver = DRIVER.Firefox()
    driver.set_window_size(size, size)
+   print("Capturing address:", address, "......")
    driver.get(address)
    element_heading = driver.find_element_by_id(id_heading)
    binary_heading = element_heading.screenshot_as_png
@@ -150,44 +167,46 @@ def save_screenshot(address):
    return graph
 
 def concatenate_graph(down, top):
-   top_alpha = top.convert("RGBA")
-   down_alpha = down.convert("RGBA")
    height_top = top.size[1]
    height_down = down.size[1]
    width = max(top.size[0], down.size[0])
-   dimension = (width, height_top + height_down)
+   height = height_top + height_down
+   dimension = (width, height)
    combined = IMAGE.new("RGBA", dimension, (255, 255, 255))
-   combined.paste(top, (0, 0), top_alpha)
-   combined.paste(down, (0, height_top), down_alpha)
+   more_width_top = (width - top.size[0]) // 2
+   more_width_down = (width - down.size[0]) // 2
+   offset_top = (more_width_top, 0)
+   offset_down = (more_width_down, top.size[1])
+   top_alpha = top.convert("RGBA")
+   down_alpha = down.convert("RGBA")
+   combined.paste(top, offset_top, mask = top_alpha)
+   combined.paste(down, offset_down, mask = down_alpha)
    combined = combined.convert("RGB")
    return combined
 
 
 def tune_leaf(leaf):
    leaf_alpha = leaf.convert("RGBA")
-   width = 1080
-   height = 1450
-   more_width = width - leaf.size[0]
-   more_height = height - leaf.size[1]
-   left_width = more_width // 2
-   left_height = more_height // 2
-   right_width = more_width - left_width
-   right_height = more_height - left_height
-   coordinate = (left_width, left_height, right_width, right_height)
-   dimension = (width, height)
+   width_outer = constant()["outer_width"]
+   height_inner = constant()["inner_height"]
+   height_outer = constant()["outer_height"]
+   more_width = (width_outer - leaf.size[0]) // 2
+   more_height = (height_outer - leaf.size[1]) // 2
+   margin_height = (height_outer - height_inner) // 2
+   more_height = max(0, min(margin_height, more_height))
+   dimension = (width_outer, height_outer)
    combined = IMAGE.new("RGBA", dimension, (255, 255, 255))
-   combined.paste(leaf, (left_width, left_height), leaf_alpha)
+   combined.paste(leaf, (more_width, more_height), mask = leaf_alpha)
    combined = combined.convert("RGB")
-   #expanded = OPERATION.expand(leaf, coordinate)
    return combined
 
 def enhance_sharpness(strip):
-   level = 2.5
+   level = 2.0
    strip = ENHANCE.Sharpness(strip).enhance(level)
    return strip
 
 def enhance_contrast(strip):
-   level = 1.5
+   level = 1.1
    strip = ENHANCE.Contrast(strip).enhance(level)
    return strip
 
@@ -258,17 +277,24 @@ def get_time_folder(folder):
    return time_folder
 
 def get_time_path(path):
-   time_path = os.stat(path).st_mtime
+   time_path = os.stat(path).st_mtime # last modified time
    return time_path
 
 def give_tail(count):
-   base = 26
-   start = 97
-   small = str(chr(count % base + start))
-   big = str(chr(count // base + start))
+   base = 26 # number of Latin alphabets
+   start = 97 # ASCII code of 'a'
+   small = chr(count % base + start)
+   big = chr(count // base + start)
    tail = str(big) + str(small)
-   return big + small
+   return tail
 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+def constant():
+   table = {
+      "inner_width": 840,
+      "inner_height": 1080,
+      "outer_width": 960,
+      "outer_height": 1360,
+      "limit_line": 300,
+   }
+   return table
 
-main()
